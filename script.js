f4ab83f5b5ab747161928aa942594cd195e549f5
tauhid-hasan <info.tauhid.cse@gmail.com>
2021-01-30 16:05:40 +0600

    Ticket Booking Project

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
 
-
-
-//First Class Function Start
-function handleTicketChange(isIncrease){
-const firstClassInput= document.getElementById('firstClassQuantity');
-const firstClassCount=parseInt(firstClassInput.value);
-let firstClassNewCount=firstClassCount;
+function handleTicketChange(whatTicket,isIncrease){
+const ClassInput= document.getElementById(whatTicket + 'ClassQuantity');
+const ClassCount=parseInt(ClassInput.value);
+let ClassNewCount=ClassCount;
 if(isIncrease == true){
-firstClassNewCount=firstClassCount + 1;
-
-}if(isIncrease == false && firstClassCount >0){
-firstClassNewCount=firstClassCount - 1;
-}
-firstClassInput.value=firstClassNewCount;
-calculateTotal();
-
-}
-
-//Economy Class Function Start
-function handleEconomyTicketChange(isIncrease){
-const economyClassInput= document.getElementById('economyClassQuantity');
-const economyClassCount=parseInt(economyClassInput.value);
-let economyClassNewCount=economyClassCount;
-if(isIncrease == true){
-economyClassNewCount=economyClassCount + 1;
+ClassNewCount=ClassCount + 1;
 
 }if(isIncrease == false && economyClassCount >0){
-economyClassNewCount=economyClassCount - 1;
+ClassNewCount=ClassCount - 1;
 }
-economyClassInput.value=economyClassNewCount;
+ClassInput.value=ClassNewCount;
 calculateTotal();
 
 }
 function setTextHtml(value, id){
 document.getElementById(id).innerText = value;
 }
+
+// //First Class Function Start
+// function handleTicketChange(isIncrease){
+// const firstClassInput= document.getElementById('firstClassQuantity');
+// const firstClassCount=parseInt(firstClassInput.value);
+// let firstClassNewCount=firstClassCount;
+// if(isIncrease == true){
+// firstClassNewCount=firstClassCount + 1;
+
+// }if(isIncrease == false && firstClassCount >0){
+// firstClassNewCount=firstClassCount - 1;
+// }
+// firstClassInput.value=firstClassNewCount;
+// calculateTotal();
+
+// }
+
+// //Economy Class Function Start
+// function handleEconomyTicketChange(isIncrease){
+// const economyClassInput= document.getElementById('economyClassQuantity');
+// const economyClassCount=parseInt(economyClassInput.value);
+// let economyClassNewCount=economyClassCount;
+// if(isIncrease == true){
+// economyClassNewCount=economyClassCount + 1;
+
+// }if(isIncrease == false && economyClassCount >0){
+// economyClassNewCount=economyClassCount - 1;
+// }
+// economyClassInput.value=economyClassNewCount;
+// calculateTotal();
+
+// }
+// function setTextHtml(value, id){
+// document.getElementById(id).innerText = value;
+// }
 
 function calculateTotal(){
 const firstClassCost= parseInt(document.getElementById('firstClassQuantity').value)*150;
